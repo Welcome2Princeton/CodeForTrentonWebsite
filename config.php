891d2bddd8faa949ad8b56335b043cf98cd01add
Kerrin McLaughlin <nirrek@live.com>
2016-01-01 21:22:15 -0500

    deleted config

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// suppress error notices
-error_reporting(E_ALL & ~E_NOTICE);
-// database credentials
-$db_user = '***REMOVED***';
-$db_password = '***REMOVED***';
-$db_host = '***REMOVED***';
-$db_name = '***REMOVED***';
-?>
