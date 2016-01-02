7ebb92f1b62038fbe2867436a8acc1bf1595cd20
Kerrin McLaughlin <nirrek@live.com>
2016-01-01 21:21:09 -0500

    semi working ajax

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// suppress error notices
+error_reporting(E_ALL & ~E_NOTICE);
+// database credentials
+$db_user = '***REMOVED***';
+$db_password = '***REMOVED***';
+$db_host = '***REMOVED***';
+$db_name = '***REMOVED***';
+?>
